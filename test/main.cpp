1881de8d745098fc2fc95667ab2a0f3f754f518b
masseda <masse.dany@gmail.com>
2014-05-16 11:45:52 +0200

    configuration de googleTest

---
+++ test/main.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : DS_Final.cpp
+// Author      : Danny
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+using namespace std;
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iomanip>
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv)
+{
+
+	::testing::InitGoogleTest(&argc,argv);
+	return RUN_ALL_TESTS();
+}
